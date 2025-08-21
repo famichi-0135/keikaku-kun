3540092d223082d3b826c45b797a13f067846220
famichi-0135 <tomop0513.maey@gmail.com>
2025-08-21 14:19:03 +0900

    ボタンに送信中のデザインを追加

--- public/main.js
+++ public/main.js
@@ -2,12 +2,20 @@
 const form = document.getElementById("submit");
 
 form.addEventListener("submit", async (event) => {
+  //ボタンを無効化
+  const button = document.querySelector(".submitButton");
+  const btnText = document.getElementById("btnText");
+  const spinner = document.getElementById("spinner");
+  button.disabled = true;
+  btnText.innerText = "送信中...";
+  spinner.style.display = "inline-block";
   event.preventDefault();
   const goalInput = document.getElementById("plan");
   const goal = goalInput.value;
   console.log(goal);
   //サーバーに対してデータを送信する。
   await sendGoalData(goal);
+  btnText.innerText = "送信";
   location.href = `/plan.html?goal=${goal}`;
 });
 
